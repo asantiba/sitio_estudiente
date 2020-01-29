56cb37072a9f6d1b0d60593b03e43d81558159c6
asantiba <alexis.santibanez.14@sansano.usm.cl>
2020-01-29 04:55:31 -0300

    vistas y rutas de profesor

--- routes/profesor.js
+++ routes/profesor.js
@@ -12,39 +12,97 @@ router.get('/', function(req, res, next) {
     }
     if(req.session.profesorLogged == true){
         // Si esta logeado va al mainframe con todas las funcionalidades
-        res.render('profesor/profesor_index', {data: req.session.profesorData});
+        res.redirect('profesor/profesor_index');
     } else {
         // Si no, lo redirige a una vista donde solo puede ver el recurso sin detalles
         res.render('profesor/profesor_login', {});
     }
 });
 
-/* Verifica si esta logeado para mostrar info de profesor en nav */
-router.post('/is_login', function(req, res, next) {
+/* Verifica si esta logeado, redirecciona a index si es asi */
+router.get('/profesor_index', function(req, res, next) {
     if(req.session.profesorLogged == true){
-        res.render('profesor/is_login', { is_login: req.session.profesorLogged, data: req.session.profesorData});
+        idprofesor = req.session.profesorData["idprofesor"].toString();
+        date = new Date()
+        semestre = "1";
+        // Si es pasado julio
+        if(date.getMonth()+1 > 7){
+            semestre = "2";
+        }
+        periodo = date.getFullYear().toString() + "-" + semestre;
+        console.log(periodo);
+        Request.get("http://52.14.108.19:8000/get_asignaturas_dictadas/" + idprofesor + "/" + periodo, (error, response, body) => {
+            if(error) {
+                return console.log(error);
+                res.render('profesor/profesor_login', { is_login: req.session.profesorLogged, data: false });
+            } else{
+                data = JSON.parse(body);
+                console.log(data);
+                res.render('profesor/profesor_index', { is_login: req.session.profesorLogged, data: data});
+            }
+        });
     } else{
-        res.render('profesor/is_login', { is_login: req.session.profesorLogged, data: false });
+        res.render('profesor/profesor_login', { is_login: req.session.profesorLogged, data: false });
     }
 });
 
-/* Renderiza verifica los datos */
-router.get('/profesor_login_confirm', function(req, res, next) {
+/* Obtiene las asignaturas de un profesor segun el periodo */
+router.get('/get_asignaturas_dictadas/:periodo', function(req, res, next) {
+    if(req.session.profesorLogged == true){
+        var periodo = req.params.periodo;
+        console.log(periodo);
+        idprofesor = req.session.profesorData["idprofesor"].toString();
+        Request.get("http://52.14.108.19:8000/get_asignaturas_dictadas/" + idprofesor + "/" + periodo, (error, response, body) => {
+            if(error) {
+                return console.log(error);
+                res.render('profesor/profesor_login', { is_login: req.session.profesorLogged, data: false });
+            } else{
+                data = JSON.parse(body);
+                console.log(data);
+                res.render('profesor/profesor_index', { is_login: req.session.profesorLogged, data: data});
+            }
+        });
+    } else{
+        res.render('profesor/profesor_login', { is_login: req.session.profesorLogged, data: false });
+    }
+});
+
+/* Desloguea al usuario */
+router.get('/profesor_logout', function(req, res, next) {
+    if(req.session.profesorLogged == true){
+        req.session.profesorLogged = false;
+        req.session.profesorData["logged"] = false;
+    } else{
+        res.redirect('/');
+    }
+});
+
+/* Verifica los datos */
+router.post('/profesor_login_confirm', function(req, res, next) {
     if(req.session.profesorLogged == false){
+        var input = JSON.parse(JSON.stringify(req.body));
         // Se hace la consulta a la api de estudiente
         try {
             Request.post({
                 "headers": { "content-type": "application/json" },
-                "url": "http://52.14.108.19:8000/profesor_login/",
+                "url": "http://52.14.108.19:8000/login_prof/",
                 "body": JSON.stringify({
-                    "correo": "",
-                    "password": ""
+                    "email": input.email,
+                    "password": input.password
                 })
             }, (error, response, body) => {
                 if(error) {
-                    return console.dir(error);
+                    return console.log(error);
+                } else{
+                    body = JSON.parse(body);
+                    if(body["logged"]){
+                        req.session.profesorLogged = true;
+                        req.session.profesorData = body;
+                        res.send({msj: "ok", data: req.session.profesorData});
+                    } else{
+                        res.send({msj: "error"});
+                    }
                 }
-                console.dir(JSON.parse(body));
             });
         } catch(error) {
             console.error(error);
@@ -52,8 +110,8 @@ router.get('/profesor_login_confirm', function(req, res, next) {
     }
 });
 
-/* Registrar profesor (para registrar a un profesor, el correo debe estar ingresado en la bd, 
-    este registro es para confirmar correo y almacenar contraseña) */
+/* Registrar profesor (para registrar a un profesor, el email debe estar ingresado en la bd, 
+    este registro es para confirmar email y almacenar contraseña) */
 router.get('/profesor_registrar', function(req, res, next) {
     if(req.session.profesorLogged != null && req.session.profesorLogged == true){
         // Si esta logeado va al mainframe con todas las funcionalidades
@@ -72,33 +130,34 @@ router.post('/profesor_registrar_confirm', function(req, res, next) {
         try {
             Request.post({
                 "headers": { "content-type": "application/json" },
-                "url": "http://52.14.108.19:8000/login_prof/",
+                "url": "http://52.14.108.19:8000/crear_usr_prof/",
                 "body": JSON.stringify({
-                    "correo": input.correo,
+                    "email": input.email,
                     "password": input.password
                 })
             }, (error, response, body) => {
                 if(error) {
                     return console.dir(error);
                 } else{
-                    data = JSON.parse(body);
+                    data = JSON.parse(JSON.stringify(body));
                     console.log(data);
                     res.send({msj: "ok", data: data});
                 }
             });
-            // Request.get("http://52.14.108.19:8000/profesor_registrar", (error, response, body) => {
-            //     if(error) {
-            //         return console.dir(error);
-            //     } else{
-            //         data = JSON.parse(body);
-            //         console.log(data);
-            //         res.send({msj: "ok", data: data});
-            //     }
-            // });
         } catch (error) {
             console.error(error);
         }
     }
 });
+
+// Request.get("http://52.14.108.19:8000/profesor_registrar", (error, response, body) => {
+//     if(error) {
+//         return console.dir(error);
+//     } else{
+//         data = JSON.parse(body);
+//         console.log(data);
+//         res.send({msj: "ok", data: data});
+//     }
+// });
 
 module.exports = router;
